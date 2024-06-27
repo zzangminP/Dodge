a5d201fbf837f96c797166576136256c74424f28
zzangmin <wjdals1212gh@gmail.com>
2024-06-27 16:01:00 +0900

    fixed untracked files

--- Assets/3.Script/ObsControl.cs
+++ Assets/3.Script/ObsControl.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class ObsControl : MonoBehaviour
 {
     public ObstacleSpawner obSpawner;
+    public Rigidbody ob_r;
+    public float rotateSpeed;
+    public float up;
 
-    /*
-    private void OnCollisionEnter(Collision col)
+
+    private void Awake()
     {
-        if (col.collider.CompareTag("Floor"))
-        {
+        ob_r = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
 
-        }
-    }*/
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
